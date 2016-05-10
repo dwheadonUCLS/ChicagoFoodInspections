b958572172757564ca50052f7aa9ee6773ead1d1
Dan Wheadon <dwheadon@ucls.uchicago.edu>
2016-05-10 13:15:40 -0500

    Merge origin/master
    Conflicts:
    	src/datagrapher/DataGrapher.java

--- src/datagrapher/DataGrapher.java
+++ src/datagrapher/DataGrapher.java
@@ -1,5 +1,4 @@
-/* foo
-/* blah
+/* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
